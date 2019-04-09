be9bf484f7277c4a7c5d1ad5fe1addbed8ad22f7
Leander Tentrup <tentrup@react.uni-saarland.de>
2019-04-09 10:54:22 +0200

    Update PackageDescription to v4

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "CAiger",
+    products: [
+        .library(name: "CAiger", targets: ["CAiger"]),
+        .library(name: "CAigerHelper", targets: ["CAigerHelper"]),
+    ],
     targets: [
-        Target(name: "CAiger"),
-        Target(name: "CAigerHelper", dependencies:["CAiger"]),
+        .target(name: "CAiger"),
+        .testTarget(name: "CAigerTests", dependencies: ["CAiger"]),
+        .target(name: "CAigerHelper", dependencies:["CAiger"]),
     ]
 )
